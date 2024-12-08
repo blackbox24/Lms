21d356879a9d89e6fb93ea0eea809452aca7f0e4
blackbox24 <nicardid@gmail.com>
2024-12-08 18:29:39 +0000

    feat: modified library api added delete api

--- library/api.py
+++ library/api.py
@@ -1,43 +1,49 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
+from validations import validate_book_data, is_valid,is_exist
 
 # create app 
 app = Flask(__name__)
 
-#  create data
-data = [
-    {'id': 546, 'username': 'John'},
-    {'id': 894, 'username': 'Mary'},
-    {'id': 326, 'username': 'Jane'}
-]
-# create routers for library
+# Fake book data
+books = [{"id": 1, "title": "John", "category": "action", "price": "$100", "author": "John Doe"}]
 
-# retrieve list of all libraries
-@app.route("/library",methods=["GET"])
-def get_library():
-    return jsonify(data)
+# Routes
+@app.route("/api/books/", methods=["GET"])
+def get_books():
+    return jsonify(books)
 
-#  retrieve library by id
-@app.route("/library/<int:id>/",methods=["GET"])
-def get_library_id(id: int):
-    for i in data:
-        if i["id"] == id:
-            return jsonify(i)
-    return jsonify({ 'error': 'Library does not exist'}), 404
-    
+@app.route("/api/books/<int:id>/", methods=["GET"])
+def get_book_by_id(id: int):
+    book = next((b for b in books if b["id"] == id), None)
+    if book:
+        return jsonify(book)
+    return jsonify({"error": "Book does not exist"}), 404
 
-# add library
-@app.route("/library",methods=["POST"])
-def add_library():
-    return "added"
+@app.route("/api/book/add/", methods=["POST"])
+def add_book():
+    try:
+        data = json.loads(request.data)
+    except json.JSONDecodeError as e:
+        return jsonify({"error": "Invalid JSON data", "message": str(e)}), 400
 
-#  delete library
-@app.route("/library",methods=["DELETE"])
-def delete_library():
-    return "deleted"
+    validated_data, status_code = validate_book_data(data, books)
+    if status_code != 200:
+        return jsonify({"error": validated_data}), status_code
 
-# update library
-@app.route("/library",methods=["PATC"])
-def update_library():
-    return "updated"
+    books.append(data)
+    return jsonify(data), 200
 
-app.run()
+# delete book
+@app.route("/api/book/remove/<int:id>/", methods=["DELETE"])
+def delete_book(id: int):
+        global books
+        if not is_exist("id",id,books):
+            return jsonify("book not found"),400
+        
+        books = [x for x in books if x["id"] != id]
+        return jsonify(books),200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
